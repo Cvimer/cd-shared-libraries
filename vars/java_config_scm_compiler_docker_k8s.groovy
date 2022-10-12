c2e4d75098f17606db1fc2c28a3489394e34494c
曹雅楠 <yanan.cao@NJ5WV33LG3.local>
2022-10-12 19:45:55 +0800

    udpate

--- vars/java_config_scm_compiler_docker_k8s.groovy
+++ vars/java_config_scm_compiler_docker_k8s.groovy
@@ -1,23 +1,13 @@
 def java_config_scm_compiler_docker_k8s() {
-    pipeline {
-        agent any
-
-        stages {
-            stage('Build') {
-                steps {
-                    echo 'Building..'
-                }
-            }
-            stage('Test') {
-                steps {
-                    echo 'Testing..'
-                }
-            }
-            stage('Deploy') {
-                steps {
-                    echo 'Deploying....'
-                }
-            }
+    node {
+        stage('Build') {
+            echo 'Building....'
+        }
+        stage('Test') {
+            echo 'Testing....'
+        }
+        stage('Deploy') {
+            echo 'Deploying....'
         }
     }
 }
